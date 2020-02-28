f2faec1ede36713cea8bddf35f84431799cf1874
maticsardi700 <ms5850@student.uni-lj.si>
2020-02-28 10:36:55 +0000

    Gravitacija-Izpis,slika

--- v1/Gravitacija.java
+++ v1/Gravitacija.java
@@ -2,4 +2,8 @@ public class Gravitacija {
     public static void main (String args) {
         System.out.print("OIs je zakon!");
     }
+    public static void izpis(double a, double b){
+        System.out.println(a);
+        System.out.println(b);
+    }
 }
