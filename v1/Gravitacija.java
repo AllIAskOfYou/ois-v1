c01c365c43d155247e08e50e1e6d6d5f0175ef46
Blazpelko <bp0759@student.uni-lj.si>
2020-02-28 10:37:06 +0000

    gravitacija-izracun,slika

--- v1/Gravitacija.java
+++ v1/Gravitacija.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
 public class Gravitacija {
+    public double izracun(double v){
+        double c =6.674*Math.pow(10,-11);
+        double M =5.972*Math.pow(10,24);
+        double r =6.371*Math.pow(10,6);
+        double sum =v+r;
+        return (c*m)/(Math.pow((sum,2);
+
+    }
     public static void main (String args) {
         System.out.print("OIs je zakon!");
     }
 }
+
+
